c4c87f6d2257eb513ac0d3cde6175c320eb9c154
Dipali <dipssonawane123@gmail.com>
2022-04-26 02:24:06 +0530

    Added all use cases files

---
+++ dicewitharrydic.sh
@@ -0,0 +1,93 @@
+#!/bin/bash -x
+
+# Write a program in the following steps
+#a. Roll a die and find the number between 1 to 6
+#b. Repeat the Die roll and find the result each time
+#c. Store the result in a dictionary
+#d. Repeat till any one of the number has reached 10 times
+#e. Find the number that reached maximum times and the one that was for minimum times
+
+declare -A show
+array=( 0 0 0 0 0 0 )
+
+for ((i=1;i<100;i++))
+do
+
+        num=$(( (RANDOM%6)+1 ))
+
+        case $num in
+          1)
+          show[dice1]=$(( (${array[0]}+1) ))
+	  array[0]=$(( (${array[0]}+1) )) 	
+          ;;
+
+          2)
+          show[dice2]=$(( (${array[1]}+1) ))
+	  array[1]=$(( (${array[1]}+1) ))
+
+          ;;
+
+          3)
+          show[dice3]=$(( (${array[2]}+1) ))
+	  array[2]=$(( (${array[2]}+1) ))
+          ;;
+
+          4)
+          show[dice4]=$(( (${array[3]}+1) ))
+	  array[3]=$(( (${array[3]}+1) ))
+          ;;
+
+          5)
+          show[dice5]=$(( (${array[4]}+1) ))
+	  array[4]=$(( (${array[4]}+1) ))
+          ;;
+
+          6)
+          show[dice6]=$(( (${array[5]}+1) ))
+	  array[5]=$(( (${array[5]}+1) ))
+          ;;
+
+        esac
+
+        if [[ ${array[0]} -eq 10 || ${array[1]}  -eq 10 || ${array[2]}  -eq 10 || ${array[3]}  -eq 10 || ${array[4]}  -eq 10 || ${array[5]}  -eq 10 ]]
+        then
+                break;
+        fi
+done
+
+temp=0
+for((i=0;i<${#array[@]};i++))
+do
+	for((j=$(($i+1));j<${#array[@]};j++))
+	do
+		if [ ${array[i]} -gt ${array[j]} ]
+		then
+			temp=${array[i]}
+			array[i]=${array[j]}
+			array[j]=$temp
+		fi
+	done
+done
+
+ 
+min=${array[0]}
+
+for num in ${!show[@]}
+do
+ echo $num " comes " ${show[$num]} " times "
+
+	if [ ${show[$num]} -eq 10 ]
+	then
+		maxdice=$num
+	fi
+
+	if [ ${show[$num]} -eq $min ]
+        then
+                mindice=$num
+        fi
+
+done
+echo " "
+echo " "
+echo $maxdice " reaches maximum times= 10"
+echo $mindice " reaches minimum times= "$min
